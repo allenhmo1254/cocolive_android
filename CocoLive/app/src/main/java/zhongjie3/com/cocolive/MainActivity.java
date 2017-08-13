e7e65dd2b3f23e4035881630d280a1d8d94a6809
zhongjie3 <zhongjie3@staff.weibo.com>
2017-08-13 14:09:38 +0800

    提交项目

---
+++ CocoLive/app/src/main/java/zhongjie3/com/cocolive/MainActivity.java
@@ -0,0 +1,13 @@
+package zhongjie3.com.cocolive;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
