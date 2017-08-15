2a66de8073242e82d037468a19e558b17bda67f4
zhongjie3 <zhongjie3@staff.weibo.com>
2017-08-15 17:49:05 +0800

    提交主界面

--- CocoLive/app/src/main/java/zhongjie3/com/cocolive/MainActivity.java
+++ CocoLive/app/src/main/java/zhongjie3/com/cocolive/MainActivity.java
@@ -1,9 +1,9 @@
 package zhongjie3.com.cocolive;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.text.Layout;
-import android.widget.Button;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v7.app.AppCompatActivity;
 import android.widget.FrameLayout;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
@@ -11,6 +11,8 @@ import android.widget.RadioGroup;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
+import zhongjie3.com.cocolive.home.HomeFragment;
+import zhongjie3.com.cocolive.me.MeFragment;
 
 public class MainActivity extends AppCompatActivity implements RadioGroup.OnCheckedChangeListener {
 
@@ -36,9 +38,7 @@ public class MainActivity extends AppCompatActivity implements RadioGroup.OnChec
 
         ButterKnife.bind(this);
 
-        //创建底部菜单标签页
-        radioGroup.setOnCheckedChangeListener(this);
-        radioGroup.check(R.id.radioHome);
+        initView();
     }
 
     private void onStartLiveClick()
@@ -48,6 +48,40 @@ public class MainActivity extends AppCompatActivity implements RadioGroup.OnChec
 
     @Override
     public void onCheckedChanged(RadioGroup group, int checkedId) {
+        FragmentManager fm = getSupportFragmentManager();
+        Fragment home = findFragmentByTag(R.id.radioHome);
+        Fragment me = findFragmentByTag(R.id.radioMe);
+        if (checkedId == R.id.radioHome) {
+            fm.beginTransaction().show(home).hide(me).commit();
+        } else if (checkedId == R.id.radioMe) {
+            fm.beginTransaction().show(me).hide(home).commit();
+        }
+        fm.executePendingTransactions();
+    }
 
+    private void initView()
+    {
+        //创建底部菜单标签页
+        radioGroup.setOnCheckedChangeListener(this);
+        radioGroup.check(R.id.radioHome);
+    }
+
+    /**
+     * 查找Home和User 两个Fragment,如果没有添加则添加
+     */
+    private Fragment findFragmentByTag(int tag) {
+        FragmentManager fm = getSupportFragmentManager();
+        Fragment f = fm.findFragmentByTag(String.valueOf(tag));
+        if (f != null) {
+            return f;
+        }
+        if (tag == R.id.radioHome) {
+            f = new HomeFragment();
+        } else if (tag == R.id.radioMe) {
+            f = new MeFragment();
+        }
+        fm.beginTransaction().add(R.id.fragmentContainer, f, String.valueOf(tag)).commit();
+        fm.executePendingTransactions();
+        return f;
     }
 }
